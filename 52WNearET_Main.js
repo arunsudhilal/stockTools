9289592a35444f858dea21772d42a52ed0dd60f2
arunsudhilal <arunsudhilal@gmail.com>
2017-07-09 13:41:42 +0530

    Working 52W near
    Signed-off-by: arunsudhilal <arunsudhilal@gmail.com>

--- 52WNearET_Main.js
+++ 52WNearET_Main.js
@@ -1,32 +1,27 @@
+var processedStocks = 0;
+var dataJson;
+var totalStocks = 0;
 $(document).ready(function(){
 	$("button").click(function(){
-		//$.getJSON("https://www.nseindia.com/products/dynaContent/equities/equities/json/online52NewHigh.json", function(result){
-		debugger
-		dataJson = JSON.parse(dataJson)
-		symbol = dataJson.searchresult[0].ticker
-		totalStocks = dataJson.searchresult.length
-		totalStocks = totalStocks -1
-		/*
-		$.getJSON(dataJson, function(result){
-			$.each(result, function(i, field){
-			totalStocks = field.length
+		if (processedStocks == 0) {
+			dataJson = JSON.parse(dataJson)
+			symbol = dataJson.searchresult[0].ticker
+			totalStocks = dataJson.searchresult.length
 			totalStocks = totalStocks - 1
 			debugger
-			while(totalStocks > 0) {
-				debugger
-				$("div").append(field[totalStocks].ticker + " ");
-				totalStocks = totalStocks - 1;
-			}
-			
-			});
-		});*/
-		while(totalStocks >= 0) {
-			$("div").append(dataJson.searchresult[totalStocks].ticker + " ");
-			totalStocks = totalStocks - 1
 		}
 		debugger
-	});
-	
+		while(totalStocks >= 0) {
+			$("div").append(dataJson.searchresult[totalStocks].ticker + " ");
+			openPage(dataJson.searchresult[totalStocks].ticker)
+			totalStocks = totalStocks - 1
+			processedStocks = processedStocks + 1
+			if(processedStocks % 5 == 0)
+				break;			
+			debugger
+		}
+		debugger
+	});	
 });
 
 function openPage(stock){
